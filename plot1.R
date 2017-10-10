08566ded222f66c624dd4f33f8e1bbecb747201a
lpost627 <lpost@monitorliability.com>
2017-10-10 13:31:54 -0500

    Delete plot1.R

--- plot1.R
+++
@@ -1,7 +0,0 @@
-setwd("C:/Users/lpost/My Documents/Coursera/04 - Exploratory Data Analysis")
-DataFile <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075258, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
-SubsetData <- subset(DataFile, Date %in% c("1/2/2007","2/2/2007"))
-SubsetData$Date <- as.Date(SubsetData$Date, format="%d/%m/%Y")
-hist(SubsetData$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="red")
-dev.copy(png, file="plot1.png")
-dev.off()
